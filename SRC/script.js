249ccc3da1334e58988a2f3632b3738a812eab58
mecachet <otoqvilitaia@gmail.com>
2024-02-08 14:29:23 +0400

    Misc update

--- SRC/script.js
+++ SRC/script.js
@@ -288,10 +288,9 @@ $(function () {
   // Existing Backbone code...
 
   window.toggle_visibility = function (element_id) {
-      var e = document.getElementById(element_id);
-      if (e.style.display == 'block' || e.style.display == '')
-          e.style.display = 'none';
-      else
-          e.style.display = 'block';
+    var e = document.getElementById(element_id);
+    if (e.style.display == "block" || e.style.display == "")
+      e.style.display = "none";
+    else e.style.display = "block";
   };
 });
